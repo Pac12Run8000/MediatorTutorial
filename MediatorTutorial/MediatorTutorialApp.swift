c4943e2429368af0e4dae6a8656c0328ec22c181
Pac12Run8000 <gangstarr78992@>
2023-05-09 09:57:35 -0700

    Initial Commit

---
+++ MediatorTutorial/MediatorTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediatorTutorialApp.swift
+//  MediatorTutorial
+//
+//  Created by Michelle Grover on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MediatorTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
